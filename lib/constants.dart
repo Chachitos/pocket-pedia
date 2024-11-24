d1e72ce492a65dd0e3a4b84c208ecddddf2307f3
binivazqua <binizavazquez@gmail.com>
2024-11-23 20:57:03 -0600

    color constants done

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final Color darkBlue = Color.fromRGBO(35, 124, 157, 1);
+final Color perryGreen = Color.fromRGBO(25, 195, 178, 1);
+final Color yellow = Color.fromRGBO(255, 203, 119, 1);
+final Color lightGrey = Color.fromRGBO(151, 151, 151, 1);
+final Color pink = Color.fromRGBO(254, 109, 115, 1);
