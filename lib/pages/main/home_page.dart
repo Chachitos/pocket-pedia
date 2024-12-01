48661ffa3177d173501956e44e324a4a6768d2a8
binivazqua <binizavazquez@gmail.com>
2024-11-30 21:39:39 -0600

    finished raw recents widget

--- lib/pages/main/home_page.dart
+++ lib/pages/main/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:pocketpedia/styles/app_colors.dart';
 import 'package:pocketpedia/widgets/material/homepage/categories.dart';
 
@@ -38,6 +39,8 @@ class HomePage extends StatelessWidget {
           ),
           Stack(
             alignment: AlignmentDirectional.bottomEnd,
+            fit: StackFit.passthrough,
+            clipBehavior: Clip.none,
             children: [
               MaterialButton(
                 onPressed: () {},
@@ -55,17 +58,54 @@ class HomePage extends StatelessWidget {
                     style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                   )),
               Container(
-                height: 100,
-                width: 150,
+                height: 80,
+                width: 120,
                 decoration: BoxDecoration(
                   shape: BoxShape.rectangle,
                   color: Colors.red,
                   borderRadius: BorderRadiusDirectional.only(
                       topEnd: Radius.elliptical(100, 20),
                       topStart: Radius.elliptical(200, 200),
-                      bottomStart: Radius.elliptical(200, 0)),
+                      bottomStart: Radius.elliptical(200, 0),
+                      bottomEnd: Radius.circular(15)),
                 ),
-              )
+              ),
+              Positioned(
+                  bottom: -30,
+                  left: 15,
+                  child: Container(
+                    width: 80,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.amber,
+                    ),
+                  )),
+              Positioned(
+                left: 15,
+                bottom: -30,
+                child: CircularPercentIndicator(
+                  radius: 40,
+                  backgroundWidth: 8,
+                  backgroundColor: Colors.black,
+                  lineWidth: 5.0,
+                  percent: 0.4,
+                  center: new Text(
+                    "40%",
+                    style: new TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 15.0),
+                  ),
+                  circularStrokeCap: CircularStrokeCap.round,
+                  progressColor: Colors.red,
+                ),
+              ),
+              Positioned(
+                  right: 12,
+                  bottom: 5,
+                  child: Image(
+                    image: AssetImage('assets/vectors/asthma_vector.png'),
+                    width: 100,
+                  )),
             ],
           )
         ],
