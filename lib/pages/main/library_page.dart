4a778fd8cb4b9563e695195f9844d6b6ab645298
binivazqua <binizavazquez@gmail.com>
2024-12-19 23:08:45 -0600

    stateful widget created

--- lib/pages/main/library_page.dart
+++ lib/pages/main/library_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:pocketpedia/styles/color/app_colors.dart';
+import 'package:pocketpedia/widgets/material/library/favorites.dart';
 
 class LibraryPage extends StatefulWidget {
   const LibraryPage({super.key});
@@ -10,6 +11,28 @@ class LibraryPage extends StatefulWidget {
 }
 
 class _LibraryPageState extends State<LibraryPage> {
+  Map<String, Map<String, dynamic>> stylesList = {
+    'Asma': {
+      "Path": 'assets/vectors/biblioteca/human_11847175.png',
+      "PColor": AppColors.red,
+      "SColor": AppColors.transparentRed,
+    },
+    'Fibrosis Quística': {
+      "Path": 'assets/vectors/biblioteca/human_11061092.png',
+      "PColor": AppColors.oceanBlue,
+      "SColor": AppColors.transparentBlue,
+    },
+    'Influenza': {
+      "Path": 'assets/vectors/biblioteca/stroke_11916674.png',
+      "PColor": AppColors.yellow,
+      "SColor": AppColors.transparentYellow,
+    },
+    'Tos crónica': {
+      "Path": ' assets/vectors/biblioteca/tissue_11916635.png',
+      "PColor": AppColors.teal,
+      "SColor": AppColors.transparentTeal,
+    }
+  };
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
@@ -17,91 +40,7 @@ class _LibraryPageState extends State<LibraryPage> {
     return SafeArea(
         child: Center(
             child: Column(
-      children: [
-        Text('Biblioteca'),
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Container(
-            height: 80,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15), color: AppColors.teal),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Stack(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: Container(
-                        width: 60,
-                        height: 60,
-                        decoration: BoxDecoration(
-                            color: AppColors.transparentTeal,
-                            borderRadius: BorderRadius.circular(50)),
-                      ),
-                    ),
-
-                    Positioned(
-                      left: 22.5,
-                      top: 9,
-                      child: Image.asset(
-                        color: Colors.white,
-                        'assets/vectors/biblioteca/human_11847175.png',
-                        width: 35,
-                        height: 35,
-                      ),
-                    )
-
-                    //Image.asset(name)
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 18.0, left: 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Asma',
-                        style: textTheme.bodyMedium,
-                      ),
-                      Text('Enfermedades alérgicas',
-                          style: TextStyle(color: Colors.white, fontSize: 12))
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 50.0),
-                  child: Stack(children: [
-                    Container(
-                      height: 60,
-                      width: 60,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(50)),
-                    ),
-                    CircularPercentIndicator(
-                      radius: 30,
-                      backgroundWidth: 8,
-                      backgroundColor: Colors.black,
-                      lineWidth: 5.0,
-                      percent: 0.4,
-                      center: new Text(
-                        "40%",
-                        style: new TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15.0),
-                      ),
-                      circularStrokeCap: CircularStrokeCap.round,
-                      progressColor: Colors.white,
-                    ),
-                  ]),
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
+      children: [Text('Biblioteca'), FavoritesLibraryState()],
     )));
   }
 }
